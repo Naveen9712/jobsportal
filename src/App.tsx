0282c6da7d63a28f3623a2a579cedc9a4bec117e
Naveen9712 <kandakurthinaveen97@gmail.com>
2025-07-16 18:39:40 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import React, { useState } from 'react';
-import DashboardSelectionPage from './pages/DashboardSelectionPage';
-import TrickyTaxPortal from './pages/TrickyTaxPortal';
-import TaxAppMockup from './pages/TaxAppMockup';
+import React from 'react';
 import './styles/App.css';
-
-type AppView = 'dashboard' | 'business' | 'personal';
+import BoroBazarTemplate from './components/borobazar';
 
 const App: React.FC = () => {
-  const [currentView, setCurrentView] = useState<AppView>('dashboard');
-
-  const handleBusinessDashboardAccess = () => {
-    setCurrentView('business');
-  };
-
-  const handlePersonalAppAccess = () => {
-    setCurrentView('personal');
-  };
-
-  const handleBackToDashboard = () => {
-    setCurrentView('dashboard');
-  };
-
-  switch (currentView) {
-    case 'business':
-      return <TrickyTaxPortal onBack={handleBackToDashboard} />;
-    case 'personal':
-      return <TaxAppMockup onBack={handleBackToDashboard} />;
-    default:
-      return (
-        <DashboardSelectionPage 
-          onBusinessDashboardAccess={handleBusinessDashboardAccess}
-          onPersonalAppAccess={handlePersonalAppAccess}
-        />
-      );
-  }
+  return <BoroBazarTemplate />;
 };
 
 export default App;
